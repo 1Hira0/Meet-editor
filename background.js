5816a10b2aa88534d62a2c4d2927359f5260c156
1Hira0 <89253787+1Hira0@users.noreply.github.com>
2022-03-17 19:11:28 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,22 +1,4 @@
-/*chrome.tabs.onCreated.addListener(function() {
-	chrome.tabs.query({
-		currentWindow: true,active: false
-		},
-	function(tab) {
-		console.log(tab)
-		for (i = 0; i < tab.length; i++) {
-			if (tab[i].title.includes("Meet – ") ) {
-				console.log("Meet tab opened")
 
-				}
-			}
-		});
-	chrome.tabs.executeScript(
-			null, {
-				file: 'myscript.js'
-			}
-		);
-	});*/
 chrome.browserAction.onClicked.addListener(function() {
 	chrome.tabs.query(
 		{currentWindow: true, active: true},
